9ff7cb38b678b207e95e701bfeb25b853d617aaa
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2025-01-03 20:48:48 +0900

    テスト実験

--- test/test.bash
+++ test/test.bash
@@ -5,9 +5,8 @@ dir=~
 
 cd $dir/ros2_ws
 rm -rf build install log
-colcon build --packages-select mypkg
 colcon build
 source $dir/.bashrc
 ls /root/ros2_ws/src/mypkg
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log
+cat /tmp/mypkg.log | grep '試行回数:1 円周率: 4.0'
