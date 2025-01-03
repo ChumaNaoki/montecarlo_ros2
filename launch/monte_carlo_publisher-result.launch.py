75b49c920c70a95c6788d500e6ae29b4c2fe55ae
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2025-01-03 22:41:51 +0900

    LICENCE

---
+++ launch/monte_carlo_publisher-result.launch.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2025 Chuma Naoki
+# SPDX-License-Identifier: BSD-3-Clause 
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+def generate_launch_description():
+
+    monte_carlo_publisher = launch_ros.actions.Node(
+            package='mypkg',
+            executable='monte_carlo_publisher',
+            )
+    result = launch_ros.actions.Node(
+            package='mypkg',
+            executable='result',
+            output='screen'
+            )
+
+    return launch.LaunchDescription([monte_carlo_publisher, result])
+
