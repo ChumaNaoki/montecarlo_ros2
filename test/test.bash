6c0f8724879e8e1418c380cb0b541220a9443e48
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2025-01-04 00:11:02 +0900

    テスト実験

--- test/test.bash
+++ test/test.bash
@@ -10,5 +10,5 @@ rm -rf build install log
 colcon build
 source $dir/.bashrc
 ls /root/ros2_ws/src/mypkg
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 10 ros2 launch mypkg monte_carlo_publisher-result.launch.py > /tmp/mypkg.log
 cat /tmp/mypkg.log | grep '試行回数:1 円周率: 0.0' || cat /tmp/mypkg.log | grep '試行回数:1 円周率: 4.0' 
